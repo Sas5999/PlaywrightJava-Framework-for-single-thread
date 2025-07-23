da14676559ebcd056a3c3eb3447e56d6ae40b095
Sas5999 <Sahilsync7900@gmail.com>
2025-07-23 14:16:26 +0530

    "Fixed config reader class"

--- src/main/java/com/makemyjourney/config/ConfigReader.java
+++ src/main/java/com/makemyjourney/config/ConfigReader.java
@@ -8,30 +8,30 @@ import java.util.Properties;
 
 public class ConfigReader {
     private static Properties props = new Properties();
-//    static {
-//        try {
-//            InputStream input = ConfigReader.class.getResourceAsStream("/Users/pdgt/IdeaProjects/PlaywrightJavaTempFramework/src/main/resources/config.properties");
-//            if (input == null) {
-//                throw new RuntimeException("Config properties not found");
-//            }
-//            props.load(input);
-//        } catch (IOException e) {
-//            throw new RuntimeException("Failed to load confir", e);
-//        }
-
-    static{ // Here reading the file where we will store the test data
+    static {
         try {
-            FileInputStream fis = new FileInputStream("/Users/pdgt/IdeaProjects/PlaywrightJavaTempFramework/src/main/resources/config.properties");
-            if (fis == null){
-                throw new RuntimeException("Config file is null");
+            InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties");
+            if (input == null) {
+                throw new RuntimeException("Config properties not found");
             }
-
-            props.load(fis);
+            props.load(input);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to load config", e);
         }
-    }
 
+//    static{ // Here reading the file where we will store the test data
+//        try {
+//            FileInputStream fis = new FileInputStream("/Users/pdgt/IdeaProjects/PlaywrightJavaTempFramework/src/main/resources/config.properties");
+//            if (fis == null){
+//                throw new RuntimeException("Config file is null");
+//            }
+//
+//            props.load(fis);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+    }
     public static String get(String key) { // Simplifying the method to avoid overwriting
         return props.getProperty(key);
     }
