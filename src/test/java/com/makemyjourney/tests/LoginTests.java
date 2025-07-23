193a21ec9c583552659243fc3f2099401e5a1141
Sas5999 <Sahilsync7900@gmail.com>
2025-07-23 11:36:15 +0530

    Initial commit -Single thread framework changes

--- src/test/java/com/makemyjourney/tests/LoginTests.java
+++ src/test/java/com/makemyjourney/tests/LoginTests.java
@@ -7,6 +7,7 @@ import io.qameta.allure.Allure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.nio.file.Paths;
@@ -14,31 +15,31 @@ import java.nio.file.Paths;
 public class LoginTests extends BaseTest {
     private static final Logger logger = LoggerFactory.getLogger(LoginTests.class);
     LoginPage loginPage ;
+    @BeforeMethod
+    public void setUpPage(){
+        loginPage = new LoginPage(page);
+    }
 
     @Test
     public void testTitle(){
         Allure.step("Check title");
-        loginPage = new LoginPage(page);
         Assert.assertEquals(page.title(),"Swag Labs");
     }
     @Test
     public void testNoCreds(){
         Allure.step("Check Empty fields");
-        loginPage = new LoginPage(page);
         loginPage.login();
         Assert.assertEquals(loginPage.getError(),"Epic sadface: Username is required");
     }
     @Test
     public void testWrongCredsl(){
         Allure.step("Check wrong creds");
-        loginPage = new LoginPage(page);
         loginPage.login("standard_user","sahif");
         Assert.assertEquals(loginPage.getError(),"Epic sadface: Username and password do not match any user in this service");
     }
     @Test
     public void testValidCred(){
         Allure.step("Test valid creds");
-        loginPage = new LoginPage(page);
         loginPage.login("standard_user","secret_sauce");
         Assert.assertEquals(page.url(),"https://www.saucedemo.com/inventory.html");
         page.screenshot(new Page.ScreenshotOptions()
